e286e233e2b1e2dd7487a7ff03a3566a21c4ff6d
Iris Li <github@iris.li>
2017-01-01 19:23:40 -0800

    Remove unused stuff leftover from yeoman generator

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,36 +4,11 @@ var Timer = require('./ui/Timer')
 var mountNode = document.getElementById('app')
 
 console.log(StellarSdk);
-var TodoList = React.createClass({
-  render: function () {
-    var createItem = function (itemText) {
-      return <li>ASDF{itemText}</li>
-    }
-    return <ul>{this.props.items.map(createItem)}</ul>
-  }
-})
+
 var TodoApp = React.createClass({
-  getInitialState: function () {
-    return {items: [], text: ''}
-  },
-  onChange: function (e) {
-    this.setState({text: e.target.value})
-  },
-  handleSubmit: function (e) {
-    e.preventDefault()
-    var nextItems = this.state.items.concat([this.state.text])
-    var nextText = ''
-    this.setState({items: nextItems, text: nextText})
-  },
   render: function () {
     return (
       <div>
-        <h3>TODO</h3>
-        <TodoList items={this.state.items} />
-        <form onSubmit={this.handleSubmit}>
-          <input onChange={this.onChange} value={this.state.text} />
-          <button>{'Add #' + (this.state.items.length + 1)}</button>
-        </form>
         <Timer />
       </div>
     )
