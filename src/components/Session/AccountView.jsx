b89a12c1b845e6e62c5f4cda93a65a9d156713a2
Iris Li <github@iris.li>
2017-01-10 10:54:23 -0800

    Show all balances in AccountView

--- src/components/Session/AccountView.jsx
+++ src/components/Session/AccountView.jsx
@@ -1,7 +1,20 @@
 const React = window.React = require('react');
+import Stellarify from '../../lib/Stellarify';
+import Printify from '../../lib/Printify';
 
 export default function AccountView({session}) {
+  console.log(session)
+  let balances = [];
+  session.account.balances.forEach(balance => {
+    console.log(balance)
+    let balanceAsset = Stellarify.asset(balance);
+    balances.push(<li key={balance.asset_issuer + balance.asset_code}>
+      {Printify.assetName(balanceAsset)}: {balance.balance}
+    </li>)
+  })
+  let balancesList = <ul>{balances}</ul>
   return <div>
     <p>Logged in as: {session.account.accountId}</p>
+    {balancesList}
   </div>
 }
