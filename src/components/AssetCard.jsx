1a14bcac851161941efec3b1919e9b2ab592b788
Iris Li <github@iris.li>
2017-01-14 12:13:44 -0800

    Add finely tuned AssetCard styling

--- src/components/AssetCard.jsx
+++ src/components/AssetCard.jsx
@@ -6,8 +6,22 @@ export default class AssetCard extends React.Component {
     super(props);
   }
   render() {
+    let asset = this.props.asset;
+
+    if (asset.isNative()) {
+      return <div className="AssetCard">lumens</div>
+    }
+
     return <div className="AssetCard">
-      {Printify.assetName(this.props.asset)}
+      <div className="AssetCard__logo">
+      </div>
+      <div className="AssetCard__content">
+        <div className="AssetCard__header">
+          <span className="AssetCard__code">{this.props.asset.getCode()}</span>
+          <a className="AssetCard__federation" href="https://stellarterm.com" target="_blank">stellarterm.com</a>
+        </div>
+        <p className="AssetCard__issuerAccountId">{this.props.asset.getIssuer()}</p>
+      </div>
     </div>
   }
 };
