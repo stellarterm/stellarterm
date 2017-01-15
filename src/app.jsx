d71b96bb0180bd57e2f23f8716b2cbbd25399522
Iris Li <github@iris.li>
2017-01-15 12:59:22 -0800

    Add better 404 page

--- src/app.jsx
+++ src/app.jsx
@@ -80,7 +80,20 @@ class TermApp extends React.Component {
         </div>
       </div>
     } else {
-      body = <div>Page not found</div>
+      body = <div className="so-back islandBack">
+        <div className="island">
+          <div className="island__header">
+            Page not found
+          </div>
+          <div className="OfferTables island__sub">
+            <div className="OfferTables__tables island__sub__division island--simplePadTB">
+              The requested page was not found. Try going to the <a href="#">home page</a> to navigate to where you want to go.
+            </div>
+            <div className="OfferTables__table island__sub__division">
+            </div>
+          </div>
+        </div>
+      </div>
     }
 
     return <div>
