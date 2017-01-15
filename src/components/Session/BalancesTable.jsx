ea6a611d15aba32b3b3781d83c6255f42dc7dc72
Iris Li <github@iris.li>
2017-01-14 17:10:32 -0800

    Add sorting to balances table

--- src/components/Session/BalancesTable.jsx
+++ src/components/Session/BalancesTable.jsx
@@ -11,40 +11,40 @@ export default class BalancesTable extends React.Component {
 
   render() {
     let account = this.props.d.session.account;
-    let balances = [];
-    account.balances.forEach(balance => {
-      let balanceAsset = Stellarify.asset(balance);
-      console.log(balance.limit)
-      let limit = balance.limit == '922337203685.4775807' ? 'maximum': balance.limit;
-      balances.push(<li key={balance.asset_issuer + balance.asset_code}>
-          <AssetCard asset={balanceAsset} fixed={true}>
-            Balance: {balance.balance} {balanceAsset.getCode()}
-            Trust limit: {limit}
-          </AssetCard>
-      </li>)
-    })
-    let balancesList = <ul>{balances}</ul>
 
-    // let trustLines = [];
-    // account.balances.forEach(balance => {
-    //   let removeButton;
-    //   if (balance.balance === '0.0000000') {
-    //     removeButton = <button onClick={() => this.props.d.handlers.removeTrust(balance.asset_code, balance.asset_issuer)}>Remove</button>
-    //   } else {
-    //     removeButton = <button disabled="true">Unable to remove trust line until balance is 0</button>
-    //   }
-    //   if (balance.asset_type !== 'native') {
-    //     let limit = balance.limit == '922337203685.4775807' ? 'unlimited': balance.limit;
-    //     trustLines.push(<li key={balance.asset_issuer + balance.asset_code}>
-    //       <span>{balance.asset_code}: Issuer: {balance.asset_issuer} Limit: {limit}</span>
-    //       {removeButton}
-    //     </li>)
-    //   }
-    // })
-    // let trustLinesList = <ul>{trustLines}</ul>
+
+    let nativeBalances = [];
+    let knownBalances = [];
+    let unknownBalances = [];
+    account.balances.forEach(balance => {
+      if (balance.asset_type === 'native') {
+        return nativeBalances.push(balance);
+      }
+      return unknownBalances.push(balance);
+    });
+
+    const reorderedBalances = nativeBalances.concat(knownBalances, unknownBalances);
+
+    let balanceCards = [];
+    reorderedBalances.forEach(balance => {
+      let balanceAsset = Stellarify.asset(balance);
+      let limit = balance.limit == '922337203685.4775807' ? 'maximum': balance.limit;
+
+      let removeLink;
+      if (balance.balance === '0.0000000') {
+        removeLink = <button onClick={() => this.props.d.handlers.removeTrust(balance.asset_code, balance.asset_issuer)}>Remove</button>
+      } else {
+        removeLink = <button disabled="true">Unable to remove trust line until balance is 0</button>
+      }
+      balanceCards.push(<AssetCard key={balance.asset_issuer + balance.asset_code} asset={balanceAsset} fixed={true}>
+        Balance: {balance.balance} {balanceAsset.getCode()}
+        Trust limit: {limit}
+        {removeLink}
+      </AssetCard>);
+    })
 
     return <div>
-      {balancesList}
+      {balanceCards}
     </div>
   }
 }
