077b3d4660e1a8d95c2a2eb198ba4ceed2cad68d
Iris Li <github@iris.li>
2017-01-15 23:24:04 -0800

    Add some styles to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,7 @@ export default class Header extends React.Component {
     return <div className="so-back HeaderBack">
       <div className="so-chunk Header">
         <nav className="Header__nav">
-          <a className="Header__nav__item" href="#">Stellarterm</a>
+          <a className="Header__nav__item Header__nav__item--logo" href="#">StellarTerm</a>
           <a className="Header__nav__item" href="#exchange">Exchange</a>
           <a className="Header__nav__item" href="#markets">Markets</a>
           <a className="Header__nav__item" href="#account">Account</a>
