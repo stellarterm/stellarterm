4d390ca8fa651a638445f469dc2121d7868f004c
Iris Li <github@iris.li>
2017-07-31 19:40:12 -0700

    Fix missing li tag

--- src/components/Session/LoginForm.jsx
+++ src/components/Session/LoginForm.jsx
@@ -61,7 +61,7 @@ export default class LoginForm extends React.Component {
               <li>Check the url to make sure you are on the correct website.</li>
               <li>Stellarterm does not save your secret key. It is stored on your browser and will be deleted once the page is refreshed or exited.</li>
               <li>For extra security, you can <a href="https://github.com/irisli/stellarterm">build from source</a> or <a href="https://github.com/stellarterm/stellarterm.github.io/">download from GitHub</a> and verify the hash.</li>
-              <li>StellarTerm is released under the Apache 2.0. It is provided "AS IS" without warranty. The developer puts a good faith effort to keep the application secure but is not responsible for any losses and activities caused by the application.
+              <li>StellarTerm is released under the Apache 2.0. It is provided "AS IS" without warranty. The developer puts a good faith effort to keep the application secure but is not responsible for any losses and activities caused by the application.</li>
             </ul>
           </div>
         </div>
