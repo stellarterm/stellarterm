9b961a902b15d7cded9de11e0fe2681c2d151949
Iris Li <github@iris.li>
2017-09-09 16:38:46 -0700

    Enforce https in anchor websites

--- src/DirectoryBuilder.js
+++ src/DirectoryBuilder.js
@@ -43,6 +43,9 @@ class DirectoryBuilder {
     if (logos[details.logo] === undefined) {
       throw new Error('Missing logo file: ' + details.logo);
     }
+    if (details.website.indexOf('http://') !== -1) {
+      throw new Error('Website URL must use https://');
+    }
     if (details.website.indexOf(details.domain) == -1) {
       throw new Error('Website URL of anchor must contain the anchor domain');
     }
