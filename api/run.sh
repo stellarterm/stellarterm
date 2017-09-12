e37d696a4235c6137208528087e88586ca1d9f71
Iris Li <github@iris.li>
2017-09-12 12:58:38 -0700

    Improve documentation for StellarTerm API

--- api/run.sh
+++
@@ -1,8 +0,0 @@
-# serverless --aws-profile stellarterm invoke --function ticker --log --data='{ "image_url": "https://assets-cdn.github.com/images/modules/open_graph/github-mark.png", "key": "github.png"}'
-rm output/v1/ticker.json
-node localRun.js
-echo '================================================================'
-echo
-echo
-cat output/v1/ticker.json
-echo
