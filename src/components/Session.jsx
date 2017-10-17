98087b58fc537e7bff3299e0154934854f4da748
Nikhil Saraf <nikhilsaraf89@gmail.com>
2017-10-17 16:08:14 -0700

    add feature flag which hides Deposit tab, you can still go to the /deposit url directly

--- src/components/Session.jsx
+++ src/components/Session.jsx
@@ -10,6 +10,8 @@ import Deposit from './Session/Deposit.jsx';
 import Generic from './Generic.jsx';
 import Loading from './Loading.jsx';
 
+const showDeposits = false;
+
 class Session extends React.Component {
   constructor(props) {
     super(props);
@@ -84,7 +86,7 @@ class Session extends React.Component {
                 <a className="subNav__nav__item" href="#account">Balances</a>
                 <a className="subNav__nav__item" href="#account/send">Send</a>
                 <a className="subNav__nav__item" href="#account/addTrust">Accept assets</a>
-                <a className="subNav__nav__item" href="#account/deposit">Deposit</a>
+                {() => showDeposits ? <a className="subNav__nav__item" href="#account/deposit">Deposit</a> : null}
               </nav>
             </div>
           </div>
