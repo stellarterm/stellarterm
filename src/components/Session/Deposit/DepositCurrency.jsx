a4f3a8db6babf40287567cad6f140d25391aa923
Nikhil Saraf <nikhilsaraf89@gmail.com>
2017-10-18 15:43:09 -0700

    added currency logos and logic to integrate into the dropdown

--- src/components/Session/Deposit/DepositCurrency.jsx
+++ src/components/Session/Deposit/DepositCurrency.jsx
@@ -1,9 +1,24 @@
 const React = window.React = require('react');
+const Assets = require('../../../assets');
 import Select from 'react-select';
 
 export default class DepositCurrency extends React.Component {
   constructor(props) {
     super(props);
+  }
+
+  _optionRenderer(option) {
+    let logo = Assets[option.value];
+    if (!logo) {
+      logo = Assets['unknown'];
+    }
+
+    return (
+      <div className="Deposit__dropdown">
+        <img className="Deposit__dropdown_img" src={logo}/>
+        <div>{option.label}</div>
+      </div>
+    );
   }
 
   render() {
@@ -18,6 +33,8 @@ export default class DepositCurrency extends React.Component {
           options={options}
           value={selectedValue}
           onChange={(newValue) => this.props.onCurrencyChange(newValue.value)}
+          optionRenderer={this._optionRenderer.bind(this)}
+          valueRenderer={this._optionRenderer.bind(this)}
         />
       </div>
     );
