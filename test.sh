84c64cc4e436d44d04ee1b6a6557a8e80bce955b
Iris Li <github@iris.li>
2017-10-24 15:13:19 -0700

    Add ticker local testing to test.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 npm run directory # Makes sure the directory is valid
 
+cd api
+echo "Test api ticker generator"
+node localTicker.js
+cd ..
+
 node test/smoke.js # Smoke test on the StellarTerm html build file
