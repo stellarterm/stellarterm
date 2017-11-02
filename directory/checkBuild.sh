a7dc2118ddcb44c1fb85baa1c89b6ffbc8d479d8
Iris Li <github@iris.li>
2017-11-02 08:26:18 -0700

    Improve order of checkBuild.sh

--- directory/checkBuild.sh
+++ directory/checkBuild.sh
@@ -1,18 +1,18 @@
 # NOTE: You must be in this directory to run this script
 
-./buildDirectory.js
 ./buildLogos.js
-
-if git diff --name-only directory.json | grep directory;
-then
-  echo 'ERROR: directory.json changes needs to be checked in';
-  exit 1
-fi;
+./buildDirectory.js
 
 if git diff --name-only logos.json | grep logos;
 then
   echo 'ERROR: logos.json changes needs to be checked in';
   exit 1
 fi;
+if git diff --name-only directory.json | grep directory;
+then
+  echo 'ERROR: directory.json changes needs to be checked in';
+  exit 1
+fi;
+
 
 openssl dgst -sha256 directory.json
