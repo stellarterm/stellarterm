d281cb464d0be7f2f4f120ec8a12200227165794
Iris Li <github@iris.li>
2017-11-02 19:07:37 -0700

    Make offertable a html table to reduce overlapping numbers

--- src/components/OfferTable.jsx
+++ src/components/OfferTable.jsx
@@ -17,28 +17,30 @@ export default function OfferTable(props) {
       <div className="OfferTable__header__item">{props.counterCurrency}</div>
       <div className="OfferTable__header__item">Depth ({props.counterCurrency})</div>
     </div>
-    {
-      _.map(props.offers, (offer, index) => {
-        let altColor = index % 2 === 0 ? '#fff' : '#f4f4f5'; // #f4f4f5 is $s-color-neutral8
-        let depthPercentage = Math.min(100, Number(offer.depth / props.maxDepth * 100).toFixed(1));
-        let rowStyle = {};
-        if (props.side === 'buy') {
-          rowStyle.background = `linear-gradient(to left, ${buyBackground} ${depthPercentage}%, ${altColor} ${depthPercentage}%)`;
-        } else {
-          rowStyle.background = `linear-gradient(to right, ${sellBackground} ${depthPercentage}%, ${altColor} ${depthPercentage}%)`;
-        }
-        return <div
-          className="OfferTable__row"
-          key={offer.key}
-          style={rowStyle}
-          onClick={() => props.d.handlers.orderbookPricePick(offer.price)}>
-          <div className="OfferTable__row__item">{Printify.lightenZeros(offer.price)}</div>
-          <div className="OfferTable__row__item">{Printify.lightenZeros(offer.base)}</div>
-          <div className="OfferTable__row__item">{Printify.lightenZeros(offer.counter)}</div>
-          <div className="OfferTable__row__item">{Printify.lightenZeros(offer.depth)}</div>
-        </div>
-      })
-    }
+    <table className="OfferTable__table"><tbody>
+      {
+        _.map(props.offers, (offer, index) => {
+          let altColor = index % 2 === 0 ? '#fff' : '#f4f4f5'; // #f4f4f5 is $s-color-neutral8
+          let depthPercentage = Math.min(100, Number(offer.depth / props.maxDepth * 100).toFixed(1));
+          let rowStyle = {};
+          if (props.side === 'buy') {
+            rowStyle.background = `linear-gradient(to left, ${buyBackground} ${depthPercentage}%, ${altColor} ${depthPercentage}%)`;
+          } else {
+            rowStyle.background = `linear-gradient(to right, ${sellBackground} ${depthPercentage}%, ${altColor} ${depthPercentage}%)`;
+          }
+          return <tr
+            className="OfferTable__row"
+            key={offer.key}
+            style={rowStyle}
+            onClick={() => props.d.handlers.orderbookPricePick(offer.price)}>
+            <td className="OfferTable__row__item">{Printify.lightenZeros(offer.price)}</td>
+            <td className="OfferTable__row__item">{Printify.lightenZeros(offer.base)}</td>
+            <td className="OfferTable__row__item">{Printify.lightenZeros(offer.counter)}</td>
+            <td className="OfferTable__row__item">{Printify.lightenZeros(offer.depth)}</td>
+          </tr>
+        })
+      }
+    </tbody></table>
   </div>
 }
 OfferTable.propTypes = {
