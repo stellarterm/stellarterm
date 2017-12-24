16814965057180fb401ea40b752e2dc0cbb483e5
Leigh Halliday <leighhalliday@gmail.com>
2017-12-24 13:14:29 -0500

    Removing unused constructors which simply passed props to the parents constructor.

--- src/components/AssetCard2.jsx
+++ src/components/AssetCard2.jsx
@@ -1,6 +1,6 @@
-const React = window.React = require('react');
-import Printify from '../lib/Printify';
-import directory from '../directory';
+const React = (window.React = require("react"));
+import Printify from "../lib/Printify";
+import directory from "../directory";
 
 // This is AssetCard2, the preferred way of displaying an asset in stellarterm.
 // The parent container should be 340px or wider
@@ -12,33 +12,48 @@ import directory from '../directory';
 // The children elements are responsible for padding within the AssetCard
 
 const decAtIndex = (input, index) => {
-  return parseInt(input.substr(index,2),16);
-}
+  return parseInt(input.substr(index, 2), 16);
+};
 const hexToRgba = (input, opacity) => {
-  return 'rgba(' + decAtIndex(input, 1) + ',' + decAtIndex(input, 3) + ',' + decAtIndex(input, 5) + ',' + BACKGROUND_OPACITY + ')';
-}
+  return (
+    "rgba(" +
+    decAtIndex(input, 1) +
+    "," +
+    decAtIndex(input, 3) +
+    "," +
+    decAtIndex(input, 5) +
+    "," +
+    BACKGROUND_OPACITY +
+    ")"
+  );
+};
 
 const BACKGROUND_OPACITY = 0.08;
 
 export default class AssetCard2 extends React.Component {
-  constructor(props) {
-    super(props);
-  }
   render() {
     if (!this.props.code) {
-      throw new Error('AssetCard2 expects to get a code in the props. Instead, got: ' + this.props.code)
+      throw new Error(
+        "AssetCard2 expects to get a code in the props. Instead, got: " +
+          this.props.code
+      );
     }
 
     let asset = {};
     if (this.props.domain !== undefined) {
       asset = directory.getAssetByDomain(this.props.code, this.props.domain);
       if (asset === null) {
-        throw new Error('AssetCard2 expects domains to point to a valid asset/anchor. Please do validation before calling AssetCard2');
+        throw new Error(
+          "AssetCard2 expects domains to point to a valid asset/anchor. Please do validation before calling AssetCard2"
+        );
       }
     } else if (this.props.issuer !== undefined) {
-      asset = directory.resolveAssetByAccountId(this.props.code, this.props.issuer);
+      asset = directory.resolveAssetByAccountId(
+        this.props.code,
+        this.props.issuer
+      );
     } else {
-      throw new Error('AssetCard2 expects to get either domain or issuer');
+      throw new Error("AssetCard2 expects to get either domain or issuer");
     }
 
     let anchor = directory.getAnchor(asset.domain);
@@ -50,38 +65,48 @@ export default class AssetCard2 extends React.Component {
       backgroundStyle.background = rgbaColor;
     }
 
-    let issuerAccountId = (asset.issuer === null) ? 'native lumens' : asset.issuer;
+    let issuerAccountId =
+      asset.issuer === null ? "native lumens" : asset.issuer;
     // Unlike AssetCard (original), this one does not link to the domain. Users can simply type it in the address bar
 
-    let assetCardMain = <div className="AssetCard2__main" style={backgroundStyle}>
-      <img className="AssetCard2__logo" src={anchor.logo}></img>
-      <div className="AssetCard2__content">
-        <div className="AssetCard2__header">
-          <span className="AssetCard2__header__code">{asset.code}</span>
-          <span className="AssetCard2__header__domain">{anchor.name}</span>
+    let assetCardMain = (
+      <div className="AssetCard2__main" style={backgroundStyle}>
+        <img className="AssetCard2__logo" src={anchor.logo} />
+        <div className="AssetCard2__content">
+          <div className="AssetCard2__header">
+            <span className="AssetCard2__header__code">{asset.code}</span>
+            <span className="AssetCard2__header__domain">{anchor.name}</span>
+          </div>
+          <p className="AssetCard2__issuerAccountId">{issuerAccountId}</p>
         </div>
-        <p className="AssetCard2__issuerAccountId">{issuerAccountId}</p>
       </div>
-    </div>
+    );
 
-    let containerClassName = 'AssetCard2';
+    let containerClassName = "AssetCard2";
 
     if (this.props.boxy) {
-      containerClassName += ' AssetCard2--boxy';
+      containerClassName += " AssetCard2--boxy";
     }
 
     if (this.props.children) {
-      containerClassName += ' AssetCard2--container';
-      return <div className={containerClassName} style={borderStyle}>
-        {assetCardMain}
-        <div className="AssetCard2__addon" style={Object.assign({}, borderStyle, backgroundStyle)}>
-          {this.props.children}
+      containerClassName += " AssetCard2--container";
+      return (
+        <div className={containerClassName} style={borderStyle}>
+          {assetCardMain}
+          <div
+            className="AssetCard2__addon"
+            style={Object.assign({}, borderStyle, backgroundStyle)}
+          >
+            {this.props.children}
+          </div>
         </div>
-      </div>
+      );
     }
 
-    return <div className={containerClassName} style={borderStyle}>
-      {assetCardMain}
-    </div>
+    return (
+      <div className={containerClassName} style={borderStyle}>
+        {assetCardMain}
+      </div>
+    );
   }
-};
+}
--- src/components/Markets.jsx
+++ src/components/Markets.jsx
@@ -1,31 +1,25 @@
-const React = window.React = require('react');
-import AssetCard from './AssetCard.jsx';
-import AssetPair from './AssetPair.jsx';
-import AssetList from './AssetList.jsx';
-import CustomMarketPicker from './CustomMarketPicker.jsx';
-import Stellarify from '../lib/Stellarify';
-import _ from 'lodash';
-
+const React = (window.React = require("react"));
+import AssetCard from "./AssetCard.jsx";
+import AssetPair from "./AssetPair.jsx";
+import AssetList from "./AssetList.jsx";
+import CustomMarketPicker from "./CustomMarketPicker.jsx";
+import Stellarify from "../lib/Stellarify";
+import _ from "lodash";
 
 export default class Markets extends React.Component {
-  constructor(props) {
-    super(props);
-  }
   render() {
     return (
       <div>
         <div className="so-back islandBack islandBack--t">
           <div className="island">
-            <div className="island__header">
-              Stellar Asset Directory
-            </div>
-            <AssetList d={this.props.d}></AssetList>
+            <div className="island__header">Stellar Asset Directory</div>
+            <AssetList d={this.props.d} />
           </div>
         </div>
         <div className="so-back islandBack">
-          <CustomMarketPicker row={true}></CustomMarketPicker>
+          <CustomMarketPicker row={true} />
         </div>
       </div>
     );
   }
-};
+}
