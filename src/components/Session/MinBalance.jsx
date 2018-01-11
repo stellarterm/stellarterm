2f553eca6a626b7da0544bab52580fa523d7d4c9
Iris Li <github@iris.li>
2018-01-11 12:28:22 -0800

    Improve minimum balance explanation

--- src/components/Session/MinBalance.jsx
+++ src/components/Session/MinBalance.jsx
@@ -27,8 +27,8 @@ export default class MinBalance extends React.Component {
     return <div>
     <div className="island__sub">
       <div className="island__sub__division MinBalance__sub">
-        <p>The Stellar network requires accounts to maintain a <a href="https://www.stellar.org/developers/guides/concepts/fees.html#minimum-account-balance">minimum balance</a>. A minimum balance is required for each entry in the account such as a trustline or offer. You can read more about this on the <a href="https://www.stellar.org/developers/guides/concepts/fees.html#minimum-account-balance">Stellar developer docs</a></p>
-        <p>Each entry (asset accepted, offer, signer) increases your minimum balance by 10 XLM. Additionally, StellarTerm enforces a 1XLM of extra minimum balance in an attempt to make sure your account can still make transactions without going below the network minimum balance requirements.</p>
+        <p>The Stellar network requires accounts to maintain a <a href="https://www.stellar.org/developers/guides/concepts/fees.html#minimum-account-balance">minimum balance</a>. A 1 XLM minimum balance is required with an additional requirement 0.5 XLM for each entry in the account such as a trustline or offer. You can read more about this on the <a href="https://www.stellar.org/developers/guides/concepts/fees.html#minimum-account-balance">Stellar developer docs</a></p>
+        <p>Each entry (asset accepted, offer, signer) increases your minimum balance by 0.5 XLM. Additionally, StellarTerm enforces a 0.1 XLM of extra minimum balance in an attempt to make sure your account can still make transactions without going below the network minimum balance requirements.</p>
         <p><strong>To decrease your minimum balance</strong>, you can remove an existing offer or <a href="#account/addTrust">unaccept an asset</a>.</p>
       </div>
       <div className="island__sub__division MinBalance__sub MinBalance__sub--table">
