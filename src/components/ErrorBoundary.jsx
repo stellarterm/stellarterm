83645901f213d467bccc4d9cea20e8458639ddb5
Iris Li <github@iris.li>
2018-01-30 14:33:35 -0800

    Add better details in ErrorBoundary

--- src/components/ErrorBoundary.jsx
+++ src/components/ErrorBoundary.jsx
@@ -26,8 +26,8 @@ export default class ErrorBoundary extends React.Component {
     if (this.state.hasError) {
       // You can render any custom fallback UI
       return <Generic title="An unknown error occured">
-        <p>An unexpected error happened. If you want to help this error get fixed, please report it to the <a href="https://github.com/irisli/stellarterm/issues" target="_blank" rel="nofollow noopener noreferrer">GitHub issue tracker</a>. DO <strong>NOT</strong> SHARE YOUR PRIVATE KEY WITH ANYONE (begins with an S).</p>
-        <p><strong>Your funds are safe</strong> and not affected by this error. This error is just a display error in StellarTerm.</p>
+        <p>An unexpected error happened. If you want to help this error get fixed, please report it to the <a href="https://github.com/irisli/stellarterm/issues" target="_blank" rel="nofollow noopener noreferrer">GitHub issue tracker</a>. Include a screenshot in the issue (you may blur out account ID and other details). DO <strong>NOT</strong> SHARE YOUR PRIVATE KEY WITH ANYONE (begins with an S).</p>
+        <p><strong>Your funds are safe</strong> and not affected by this error. This error is just a display error in StellarTerm, and is not your fault.</p>
         <pre>
           {this.state.error}{'\n'}
           {this.state.stack}
