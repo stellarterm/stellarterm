6db2a5b185fa2f39f71b9629d18b9817c600b14d
Iris Li <github@iris.li>
2018-02-05 20:42:50 -0800

    Redesign account login page

--- src/components/Session/LoginPage.jsx
+++ src/components/Session/LoginPage.jsx
@@ -19,6 +19,7 @@ export default class LoginPage extends React.Component {
       show: false,
       invalidKey: false,
       newKeypair: null,
+      currentTab: 'login', // 'login', 'createAccount'
     }
 
     this.handleInput = (event) => {
@@ -27,6 +28,9 @@ export default class LoginPage extends React.Component {
     this.toggleShow = (event) => {
       event.preventDefault();
       this.setState({show: !this.state.show});
+    }
+    this.setTab = (tabName) => {
+      this.setState({currentTab: tabName});
     }
     this.handleSubmit = (event) => {
       event.preventDefault();
@@ -67,40 +71,40 @@ export default class LoginPage extends React.Component {
 
     let inputType = this.state.show ? 'text' : 'password';
 
-    return <div className="so-back islandBack islandBack--t">
-      <div className="island island--pb">
-        <div>
-          <div className="island__header">
-            Log in
-          </div>
-          <div className="LoginPage">
-            <div className="LoginPage__form">
-              <p className="LoginPage__intro">Log in with your secret key to manage your account.</p>
-              <form onSubmit={this.handleSubmit}>
-                <label className="s-inputGroup LoginPage__inputGroup">
-                  <input type={inputType} className="s-inputGroup__item S-flexItem-share LoginPage__password" value={this.state.secretInput} onChange={this.handleInput} placeholder="Secret key (example: SBSMVCIWBL3HDB7N4EI3QKBKI4D5ZDSSDF7TMPB.....)" />
-                  <div>
-                    <a className="LoginPage__show s-button s-button--light" onClick={this.toggleShow}>Show</a>
-                  </div>
-                </label>
-                {errorMessage}
+    let body;
+
+    if (this.state.currentTab === 'login') {
+      body = <div className="LoginPage__body">
+        <div className="LoginPage__box">
+          <div className="LoginPage__form">
+            <p className="LoginPage__intro">Log in with your secret key to manage your account.</p>
+            <form onSubmit={this.handleSubmit}>
+              <label className="s-inputGroup LoginPage__inputGroup">
+                <input type={inputType} className="s-inputGroup__item S-flexItem-share LoginPage__password" value={this.state.secretInput} onChange={this.handleInput} placeholder="Secret key (example: SBSMVCIWBL3HDB7N4EI3QKBKI4D5ZDSSDF7TMPB.....)" />
                 <div>
-                  <input type="submit" className="LoginPage__submit s-button" value="Log in"></input>
+                  <a className="LoginPage__show s-button s-button--light" onClick={this.toggleShow}>Show</a>
                 </div>
-              </form>
-            </div>
-            <div className="LoginPage__notes">
-              <h3>Security notes</h3>
-              <ul>
-                <li>Check the url to make sure you are on the correct website.</li>
-                <li>Stellarterm does not save your secret key. It is stored on your browser and will be deleted once the page is refreshed or exited.</li>
-                <li>For extra security, you can <a href="https://github.com/irisli/stellarterm" target="_blank" rel="nofollow noopener noreferrer">build from source</a> or <a href="https://github.com/stellarterm/stellarterm.github.io/" target="_blank" rel="nofollow noopener noreferrer">download from GitHub</a> and verify the hash.</li>
-                <li>StellarTerm is released under the Apache 2.0. It is provided "AS IS" without warranty. The developer is not responsible for any losses and activities caused by the application.</li>
-              </ul>
-            </div>
+              </label>
+              {errorMessage}
+              <div>
+                <input type="submit" className="LoginPage__submit s-button" value="Log in"></input>
+              </div>
+            </form>
+          </div>
+          <div className="LoginPage__notes">
+            <h3>Security notes</h3>
+            <ul>
+              <li>Check the url to make sure you are on the correct website.</li>
+              <li>Stellarterm does not save your secret key. It is stored on your browser and will be deleted once the page is refreshed or exited.</li>
+              <li>For extra security, you can <a href="https://github.com/irisli/stellarterm" target="_blank" rel="nofollow noopener noreferrer">build from source</a> or <a href="https://github.com/stellarterm/stellarterm.github.io/" target="_blank" rel="nofollow noopener noreferrer">download from GitHub</a> and verify the hash.</li>
+              <li>StellarTerm is released under the Apache 2.0. It is provided "AS IS" without warranty. The developer is not responsible for any losses and activities caused by the application.</li>
+            </ul>
           </div>
         </div>
-        <div className="LoginPage">
+      </div>
+    } else if (this.state.currentTab === 'createAccount') {
+      body = <div className="LoginPage__body">
+        <div className="LoginPage__box">
           <div className="LoginPage__form">
             <h3>Create Account Keypair</h3>
             <p>To get started on using the Stellar network, you must first create a keypair. The keypair consists of two parts:</p>
@@ -115,6 +119,25 @@ export default class LoginPage extends React.Component {
             <h3>Account generation security notes</h3>
             <p>The key is generated using entropy from <a href="https://github.com/dchest/tweetnacl-js#random-bytes-generation">TweetNaCl's randomByte function</a> which, in most browsers, uses getRandomValues from the <a href="https://w3c.github.io/webcrypto/Overview.html">Web Cryptography API</a>. However, using a secure random number generation does not protect you from a compromised computer. Take great care to make sure your computer is secure and do not run this on a computer you do not trust.</p>
           </div>
+        </div>
+      </div>
+    }
+
+    return <div className="so-back islandBack islandBack--t">
+      <div className="island">
+        <div className="island__header">
+          Access your account
+        </div>
+        <div className="LoginPage">
+          <div className="LoginPage__sidebar">
+            <a className={'LoginPage__sidebar__tab' + (this.state.currentTab === 'login' ? ' is-active' : '')} onClick={() => {this.setTab('login')}}>
+              Log in
+            </a>
+            <a className={'LoginPage__sidebar__tab' + (this.state.currentTab === 'createAccount' ? ' is-active' : '')} onClick={() => {this.setTab('createAccount')}}>
+              Create account
+            </a>
+          </div>
+          {body}
         </div>
       </div>
     </div>
