a5ea30fdfc4b40b2e3b40e7454612f7afd0e34e4
Iris Li <github@iris.li>
2018-02-08 18:43:28 -0800

    Reduce dependencies in package.json

--- src/components/Session/Deposit/DepositCurrency.jsx
+++ src/components/Session/Deposit/DepositCurrency.jsx
@@ -1,5 +1,5 @@
 const React = window.React = require('react');
-import Select from 'react-select';
+// import Select from 'react-select';
 
 export default class DepositCurrency extends React.Component {
   constructor(props) {
@@ -18,18 +18,18 @@ export default class DepositCurrency extends React.Component {
     const codes = Array.from(new Set(this.props.allAssets.map(asset => asset.code)));
     const options = codes.map(code => ({ value: code, label: code }));
     const selectedValue = { value: this.props.selectedAsset, label: this.props.selectedAsset };
-    
-    return (
-      <div>
-        <Select
-          clearable={false}
-          options={options}
-          value={selectedValue}
-          onChange={(newValue) => this.props.onCurrencyChange(newValue.value)}
-          optionRenderer={this.optionRenderer}
-          valueRenderer={this.optionRenderer}
-        />
-      </div>
-    );
+    return <div>Feature not active</div>
+    // return (
+    //   <div>
+    //     <Select
+    //       clearable={false}
+    //       options={options}
+    //       value={selectedValue}
+    //       onChange={(newValue) => this.props.onCurrencyChange(newValue.value)}
+    //       optionRenderer={this.optionRenderer}
+    //       valueRenderer={this.optionRenderer}
+    //     />
+    //   </div>
+    // );
   }
 }
