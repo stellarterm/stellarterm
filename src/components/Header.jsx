cbc10444c4b2104a2eb1fc66b609274d43593520
Iris Li <github@iris.li>
2018-02-08 20:17:26 -0800

    Improve usability of header by making links bigger and add hover state

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -18,16 +18,17 @@ export default class Header extends React.Component {
         </div>
       </div>
     }
+    console.log(this.props.urlParts[0])
     return <div className="HeaderBackBack">
       {networkBar}
       <div className="so-back HeaderBack">
         <div className="so-chunk Header">
           <nav className="Header__nav">
             <a className="Header__nav__item Header__nav__item--logo" href="#">StellarTerm</a>
-            <a className="Header__nav__item" href="#exchange">Exchange</a>
-            <a className="Header__nav__item" href="#markets">Markets</a>
-            <a className="Header__nav__item" href="#account">Account</a>
-            <a className="Header__nav__item" href="#download">Download</a>
+            <a className={'Header__nav__item Header__nav__item--link' + (this.props.urlParts[0] === 'exchange' ? ' is-current' : '')} href="#exchange">Exchange</a>
+            <a className={'Header__nav__item Header__nav__item--link' + (this.props.urlParts[0] === 'markets' ? ' is-current' : '')} href="#markets">Markets</a>
+            <a className={'Header__nav__item Header__nav__item--link' + (this.props.urlParts[0] === 'account' ? ' is-current' : '')} href="#account">Account</a>
+            <a className={'Header__nav__item Header__nav__item--link' + (this.props.urlParts[0] === 'download' ? ' is-current' : '')} href="#download">Download</a>
           </nav>
           <span className="Header__version">v{window.stBuildInfo.version}</span>
         </div>
