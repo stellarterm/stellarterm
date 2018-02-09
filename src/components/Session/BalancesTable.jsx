6deef6bf671338261e2cc2d75ee23e6c8f1924a8
Iris Li <github@iris.li>
2018-02-09 02:10:31 -0800

    Show USD value in account page

--- src/components/Session/BalancesTable.jsx
+++ src/components/Session/BalancesTable.jsx
@@ -16,19 +16,37 @@ export default class BalancesTable extends React.Component {
     let account = this.props.d.session.account;
     let allBalances = account.getSortedBalances(); // From MagicSpoon.Account
     let balanceRows = allBalances.map(balance => {
+      let balanceUSD;
+      let tradeLink;
+      if (this.props.d.ticker.ready) {
+        let tickerAsset = _.find(this.props.d.ticker.data.assets, {
+          code: balance.code,
+          issuer: balance.issuer,
+        });
+        if (tickerAsset) {
+          balanceUSD = '$' + Math.round(balance.balance * tickerAsset.price_USD, 2).toLocaleString('en-US', {minimumFractionDigits: 2});
+          if (tickerAsset.slug !== 'XLM-native') {
+            tradeLink = <a href={'#markets/' + tickerAsset.topTradePairSlug} className="BalancesTable__row__trade">trade</a>
+          }
+        }
+      }
       return <tr className="BalancesTable__row" key={balance.code + balance.issuer}>
         <td className="BalancesTable__row__item BalancesTable__row__item--assetCard">
           <AssetCard2 code={balance.code} issuer={balance.issuer}></AssetCard2>
         </td>
         <td className="BalancesTable__row__item BalancesTable__row__item--amount">{Printify.lightenZeros(balance.balance)}</td>
+        <td className="BalancesTable__row__item BalancesTable__row__item--amount">{balanceUSD}</td>
+        <td className="BalancesTable__row__item BalancesTable__row__item--amount">{tradeLink}</td>
       </tr>
     });
 
     return <table className="BalancesTable">
       <thead>
         <tr className="BalancesTable__head">
-          <td className="BalancesTable__head__cell BalancesTable__head__asset">Asset</td>
-          <td className="BalancesTable__head__cell BalancesTable__head__amount">Balance</td>
+          <td className="BalancesTable__head__cell BalancesTable__row__item--heading BalancesTable__head__asset">Asset</td>
+          <td className="BalancesTable__head__cell BalancesTable__row__item--heading BalancesTable__head__amount">Balance</td>
+          <td className="BalancesTable__head__cell BalancesTable__row__item--heading BalancesTable__head__amount">Value (USD)</td>
+          <td className="BalancesTable__head__cell BalancesTable__row__item--heading BalancesTable__head__amount"></td>
         </tr>
       </thead>
       <tbody>
