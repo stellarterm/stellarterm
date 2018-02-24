c5a57aaf913d7552b52af85aa8bcf15f8d645f59
Iris Li <github@iris.li>
2018-02-24 13:17:36 -0800

    Use bid for price when spread is wide

--- src/components/PairPicker.jsx
+++ src/components/PairPicker.jsx
@@ -36,6 +36,9 @@ export default class PairPicker extends React.Component {
             dataRow = <p className="PairPicker__infoBar__ticker__data">{Format.niceRound(latestPrice)} XLM/{counterSelling.getCode()}</p>
           } else {
             let latestXLM = Format.niceRound(latestPrice);
+            if (1 - this.props.d.orderbook.data.bids[0].price/this.props.d.orderbook.data.asks[0].price > 0.4) {
+              latestXLM = Format.niceRound(this.props.d.orderbook.data.bids[0].price);
+            }
             let latestUSD = Format.niceRound(latestXLM * this.props.d.ticker.data._meta.externalPrices.USD_XLM);
             dataRow = <p className="PairPicker__infoBar__ticker__data">{latestXLM} XLM<span className="PairPicker__infoBar__ticker__spacer">&nbsp;</span>${latestUSD}</p>
           }
