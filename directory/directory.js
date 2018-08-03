eb11e6153e5878ad997fdffc9d32ac6f01bd638c
jlilly <johannlilly@users.noreply.github.com>
2018-08-03 02:47:28 -0400

    add pair

--- directory/directory.js
+++ directory/directory.js
@@ -680,7 +680,18 @@ directory.addWildcard('pr.network', {
   issuer: 'GAZPKDTEZ5UM3BF4E7FL7EMXRMLH76F2TNVXRLOF6SCVXOFWSPCEWFI5',
 });
 
-
+directory.addAnchor({
+  domain: 'woolf.university',
+  website: 'https://woolf.university',
+  logo: 'woolf.university',
+  color: '#245773',
+  displayName: 'WOOLF',
+});
+directory.addAsset('GBRH4SJER4R7MXH5GYSE3WXKPMS53CQ22TVA7X64SDDWKABOPRN6JMP4', {
+  code: 'WOOLF',
+  issuer: 'GBRH4SJER4R7MXH5GYSE3WXKPMS53CQ22TVA7X64SDDWKABOPRN6JMP4',
+  type: 'token',
+});
 
 
 // Pairs involving "fiat" assets
@@ -957,6 +968,10 @@ directory.addPair({
 });
 directory.addPair({
   baseBuying: ['SIX', 'six.network'],
+  counterSelling: ['XLM', 'native'],
+});
+directory.addPair({
+  baseBuying: ['WOOLF', 'woolf.university'],
   counterSelling: ['XLM', 'native'],
 });
 
