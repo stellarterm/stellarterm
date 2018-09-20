31a1d6ba8a38e7cc8e866e1a82562e17f485213e
Iris Li <github@iris.li>
2018-09-20 08:50:29 -0700

    Fix shutdown notice date

--- src/components/ShutDown.jsx
+++ src/components/ShutDown.jsx
@@ -8,11 +8,11 @@ export default class ShutDown extends React.Component {
   render() {
     return <div className={'ShutDown ' + this.props.className}>
     <div className="ShutDown__inside s-alert s-alert--warning">
-      <h2 className="ShutDown__title">StellarTerm is shutting down on September 31 2018.</h2>
+      <h2 className="ShutDown__title">StellarTerm is shutting down on October 1 2018.</h2>
       <br />
       <p>Development of StellarTerm is unable to keep up with the latest developments of the Stellar network and can't provide the best experience that users all deserve. Unfortunately, due to lack of funding and dev time, StellarTerm will be shutting down.</p>
 
-      <p>On September 31 2018, the web app and StellarTerm API will no longer be available. For alternatives, consider checking out <a href="https://stronghold.co/">Stronghold</a>, <a href="https://www.stellarx.com/">stellarx</a>, or the other wallets listed on https://www.stellar.org/lumens/wallets/. StellarTerm is not affiliated with these services.</p>
+      <p>On October 1 2018, the web app and StellarTerm API will no longer be available. For alternatives, consider checking out <a href="https://stronghold.co/">Stronghold</a>, <a href="https://www.stellarx.com/">stellarx</a>, or the other wallets listed on https://www.stellar.org/lumens/wallets/. StellarTerm is not affiliated with these services.</p>
 
       <p>Thank you for all your support. Onwards.</p>
     </div></div>
