0df6ccfa6c76f97fbf72c1e93b2ea171eb93202c
flat-resonance <flat-resonance@stellarterm.com>
2019-01-18 15:08:48 +0300

    Set stage dynamically

--- api/deploy.sh
+++ api/deploy.sh
@@ -9,5 +9,21 @@ if [ -z "$STELLARTERM_AWS_PROFILE" ]; then
     exit 1
 fi
 
+BRANCH="$(git branch | grep \* | cut -d ' ' -f2)"
+
+if [ $BRANCH != "master" ] && [ $BRANCH != "staging" ]
+then
+    echo "You must be at staging or master branch."
+    exit 1
+fi
+
+if [ $BRANCH == "master" ]
+then
+    BRANCH="dev"
+fi
+
+echo "Starting API deploy"
+echo "Stage for deploy is $BRANCH"
+
 serverless --aws-profile $STELLARTERM_AWS_PROFILE deploy
 # serverless --aws-profile $STELLARTERM_AWS_PROFILE deploy function -f ticker
