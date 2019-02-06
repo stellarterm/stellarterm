937a086d78f245886392ed58040b6ae1f7c68034
little-wave <little-wave@lobstr.co>
2019-02-06 13:42:56 +0300

    Code refactor, undefined balance view fix

--- src/components/Session/AccountView.jsx
+++ src/components/Session/AccountView.jsx
@@ -1,36 +1,34 @@
-const React = window.React = require('react');
-import Stellarify from '../../lib/Stellarify';
-import Printify from '../../lib/Printify';
-import BalancesTable from './BalancesTable.jsx';
-import MinBalance from './MinBalance.jsx';
-import _ from 'lodash';
+import React from 'react';
+import PropTypes from 'prop-types';
+import Driver from '../../lib/Driver';
 
-export default class AccountView extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+import BalancesTable from './BalancesTable';
+import MinBalance from './MinBalance';
 
-  render() {
-    return <div>
-      <div className="so-back islandBack">
-        <div className="island">
-          <div className="island__header">
-            Balances
-          </div>
-          <div className="Session__AccountView__content">
-            By default, your account is only configured to accept <strong>XLM</strong>. In order to receive other assets, you must <a href="#account/addTrust">accept</a> them using the <strong>Accept assets</strong> tool.
-          </div>
-          <BalancesTable d={this.props.d}></BalancesTable>
+export default function AccountView(props) {
+    return (
+        <div>
+            <div className="so-back islandBack">
+                <div className="island">
+                    <div className="island__header">Balances</div>
+                    <div className="Session__AccountView__content">
+                        By default, your account is only configured to accept <strong>XLM</strong>. In order to receive
+                        other assets, you must <a href="#account/addTrust">accept</a> them using the{' '}
+                        <strong>Accept assets</strong> tool.
+                    </div>
+                    <BalancesTable d={props.d} />
+                </div>
+            </div>
+            <div className="so-back islandBack">
+                <div className="island">
+                    <div className="island__header">Minimum Balance</div>
+                    <MinBalance d={props.d} />
+                </div>
+            </div>
         </div>
-      </div>
-      <div className="so-back islandBack">
-        <div className="island">
-          <div className="island__header">
-            Minimum Balance
-          </div>
-          <MinBalance d={this.props.d}></MinBalance>
-        </div>
-      </div>
-    </div>
-  }
+    );
 }
+
+AccountView.propTypes = {
+    d: PropTypes.instanceOf(Driver).isRequired,
+};
