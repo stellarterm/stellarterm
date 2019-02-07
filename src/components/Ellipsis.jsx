848a7ad0fb7cb9db66ad3d2eb96b839e521c92e1
little-wave <little-wave@lobstr.co>
2019-02-07 16:12:59 +0300

    Components code refactor, propTypes adding, some loops simplify

--- src/components/Ellipsis.jsx
+++ src/components/Ellipsis.jsx
@@ -1,35 +1,49 @@
-const React = window.React = require('react');
+import React from 'react';
 
 const TOTAL_DOTS = 3;
+
 export default class Ellipsis extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      visibleDots: 0, // 0 indexed
+    constructor(props) {
+        super(props);
+        this.state = {
+            visibleDots: 0, // 0 indexed
+        };
+
+        this.tick = () => {
+            if (this.mounted) {
+                this.setState({
+                    visibleDots: (this.state.visibleDots + 1) % (TOTAL_DOTS + 1),
+                });
+                setTimeout(this.tick, 300);
+            }
+        };
     }
 
-    this.tick = () => {
-      if (this.mounted) {
-        this.setState({
-          visibleDots: (this.state.visibleDots + 1) % (TOTAL_DOTS + 1),
-        });
+    componentDidMount() {
+        this.mounted = true;
         setTimeout(this.tick, 300);
-      }
     }
-  }
-  componentDidMount() {
-    this.mounted = true;
-    setTimeout(this.tick, 300);
-  }
-  componentWillUnmount() {
-    this.mounted = false;
-  }
-  render() {
-    let dots = [];
-    for (let i = 0; i < TOTAL_DOTS; i++) {
-      dots.push(<span key={i} className={'Ellipsis__dot' + (i >= this.state.visibleDots ? ' is-hidden' : '')}>.</span>)
-    }
-    return <span className="Ellipsis">{dots}</span>
-  }
-}
 
+    componentWillUnmount() {
+        this.mounted = false;
+    }
+
+    createDots() {
+        const dots = [];
+        let dotsNum = 0;
+
+        while (dotsNum < TOTAL_DOTS) {
+            dots.push(
+                <span key={dotsNum} className={`Ellipsis__dot${dotsNum >= this.state.visibleDots ? ' is-hidden' : ''}`}>
+                    .
+                </span>,
+            );
+            dotsNum += 1;
+        }
+        return dots;
+    }
+
+    render() {
+        return <span className="Ellipsis">{this.createDots()}</span>;
+    }
+}
