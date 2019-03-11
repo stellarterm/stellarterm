cb3f0195966cd905fd38c1a1492f68ef338f4ba5
Gleb Pitsevich <pitsevich@gmail.com>
2019-03-11 12:11:49 +0300

    Improving texts for multi-sig pop-up

--- src/components/MultisigSubmitModal.jsx
+++ src/components/MultisigSubmitModal.jsx
@@ -9,14 +9,14 @@ export default function MultisigSubmitModal(props) {
     return (
         <div className="MultisigSubmitModal ">
             <div className="MultisigSubmitModal__header">
-                Sign transaction with your {signer.title}
+                Sign transaction with {signer.title}
             </div>
             <div className="MultisigSubmitModal__content">
                 <img
                     src={images[signer.logo]}
                     className="MultisigSubmitModal__logo"
                     alt={signer.title} />
-                <span>Transaction has been sent to your {signer.title}</span>
+                <span>Transaction has been sent to {signer.title}</span>
             </div>
             <div className="MultisigSubmitModal__navigation">
                 <button className="MultisigSubmitModal__button" onClick={() => submit.cancel()}>Ok</button>
