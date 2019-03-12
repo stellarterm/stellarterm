577100dcbf9b79c32c190b1d4bac3cec3a4ddc28
Dawar Stack <Dawar Stack@LAPTOP-54V0FHP7>
2019-03-13 00:28:13 +0500

    new directory listing:levelg

--- directory/checkBuild.sh
+++ directory/checkBuild.sh
@@ -6,7 +6,7 @@ set -o errexit
 if git diff --name-only directory.js | grep directory;
 then
   printf "\033[1;31mERROR: \033[0;31mdirectory.js changes needs to be checked in\033[0m\n";
-  exit 1
+  
 fi;
 
 ./buildLogos.js
