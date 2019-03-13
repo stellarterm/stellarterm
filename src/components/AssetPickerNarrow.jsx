ae0137c559d6e162c8c81e51bd6e02b83944a7d9
little-wave <little-wave@lobstr.co>
2019-03-13 17:24:39 +0300

    Components refactored, some views optimized

--- src/components/AssetPickerNarrow.jsx
+++ src/components/AssetPickerNarrow.jsx
@@ -1,65 +1,88 @@
-const React = window.React = require('react');
+import React from 'react';
+import PropTypes from 'prop-types';
 import _ from 'lodash';
 import Validate from '../lib/Validate';
 import directory from '../directory';
 
-
 export default class AssetPickerNarrow extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      code: '',
-      issuer: '',
+    static checkInputs({ inputCode: code, inputIssuer: issuer }) {
+        const isXlmNative = code === 'XLM' && issuer === '';
+
+        if (isXlmNative) {
+            return StellarSdk.Asset.native();
+        }
+
+        const assetByAccountId = directory.getAssetByAccountId(code, issuer);
+        if (assetByAccountId !== null) {
+            return new StellarSdk.Asset(assetByAccountId.code, assetByAccountId.issuer);
+        }
+
+        const assetByDomain = directory.getAssetByDomain(code, issuer);
+        if (assetByDomain !== null) {
+            return new StellarSdk.Asset(assetByDomain.code, assetByDomain.issuer);
+        }
+
+        const isValidAsset = Validate.publicKey(issuer).ready && Validate.assetCode(code);
+        if (isValidAsset) {
+            return new StellarSdk.Asset(code, issuer);
+        }
+        return null;
     }
 
-    this.handleInput = (type, e) => {
-      e.preventDefault();
-      let newState = _.assign({}, this.state)
-      if (type === 'code') {
-        newState.code = e.target.value;
-      } else {
-        newState.issuer = e.target.value;
-      }
-      this.setState(newState)
-      let result = this.checkInputs(newState);
-      this.props.onUpdate(result);
-    }
-  }
-  checkInputs(newState) {
-    if (newState.code === 'XLM' && newState.issuer === '') {
-      return StellarSdk.Asset.native();
+    constructor(props) {
+        super(props);
+        this.state = {
+            inputCode: '',
+            inputIssuer: '',
+        };
     }
 
-    let assetByAccountId = directory.getAssetByAccountId(newState.code, newState.issuer);
-    if (assetByAccountId !== null) {
-      return new StellarSdk.Asset(assetByAccountId.code, assetByAccountId.issuer);
+    getAssetPickerInput(isCode) {
+        const { inputCode, inputIssuer } = this.state;
+
+        return (
+            <label className="s-inputGroup AssetPickerNarrow" htmlFor={isCode ? 'inputCode' : 'inputIssuer'}>
+                <span className="s-inputGroup__item AssetPickerNarrow_tag s-inputGroup__item--tag S-flexItem-full">
+                    <span>{isCode ? 'Asset Code' : 'Issuer Account ID or federation'}</span>
+                </span>
+                <input
+                    className="s-inputGroup__item S-flexItem-full"
+                    type="text"
+                    name={isCode ? 'inputCode' : 'inputIssuer'}
+                    value={isCode ? inputCode : inputIssuer}
+                    onChange={e => this.handleInput(e)}
+                    placeholder={
+                        isCode
+                            ? 'example: XLM'
+                            : 'example: naobtc.com or GC4DJYMFQZVX3R56FVCN3WA7FJFKT24VI67ODTZUENSE4YNUXZ3WYI7R'
+                    } />
+            </label>
+        );
     }
 
-    let assetByDomain = directory.getAssetByDomain(newState.code, newState.issuer);
-    if (assetByDomain !== null) {
-      return new StellarSdk.Asset(assetByDomain.code, assetByDomain.issuer);
+    handleInput({ target }) {
+        const newState = _.assign({}, this.state);
+        const { name, value } = target;
+
+        if (name === 'inputCode') {
+            newState.inputCode = value.toUpperCase();
+        } else {
+            newState.inputIssuer = value;
+        }
+        this.setState(newState);
+        this.props.onUpdate(this.constructor.checkInputs(newState));
     }
 
-    if (Validate.publicKey(newState.issuer).ready && Validate.assetCode(newState.code)) {
-      return new StellarSdk.Asset(newState.code, newState.issuer);
+    render() {
+        return (
+            <React.Fragment>
+                {this.getAssetPickerInput(true)}
+                {this.getAssetPickerInput(false)}
+            </React.Fragment>
+        );
     }
+}
 
-    return null;
-  }
-  render() {
-    return <div>
-      <label className="s-inputGroup AssetPickerNarrow__inputGroup AssetPickerNarrow__code">
-        <span className="s-inputGroup__item AssetPickerNarrow__tag s-inputGroup__item--tag S-flexItem-full">
-          <span>Asset Code</span>
-        </span>
-        <input className="s-inputGroup__item AssetPickerNarrow__input S-flexItem-full" type="text" value={this.state.code} onChange={e => this.handleInput('code', e)} placeholder="example: XLM" />
-      </label>
-      <label className="s-inputGroup AssetPickerNarrow__inputGroup AssetPickerNarrow__issuer">
-        <span className="s-inputGroup__item AssetPickerNarrow__tag s-inputGroup__item--tag S-flexItem-full">
-          <span>Issuer Account ID or federation</span>
-        </span>
-        <input className="s-inputGroup__item AssetPickerNarrow__input S-flexItem-full" type="text" value={this.state.issuer} onChange={e => this.handleInput('issuer', e)} placeholder="example: naobtc.com or GC4DJYMFQZVX3R56FVCN3WA7FJFKT24VI67ODTZUENSE4YNUXZ3WYI7R" />
-      </label>
-    </div>
-  }
+AssetPickerNarrow.propTypes = {
+    onUpdate: PropTypes.func.isRequired,
 };
