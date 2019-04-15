354307925413a069d2d8a91d4cd7448e3c68db76
little-wave <little-wave@lobstr.co>
2019-04-15 13:40:42 +0300

    orderBook refactored

--- src/lib/driver/Orderbook.js
+++ src/lib/driver/Orderbook.js
@@ -1,42 +1,38 @@
-import _ from 'lodash';
-
 import MagicSpoon from '../MagicSpoon';
-import Stellarify from '../Stellarify';
-import directory from '../../directory';
-import Validate from '../Validate';
 import Event from '../Event';
 
 export default function Orderbook(driver) {
-  this.event = new Event();
+    this.event = new Event();
 
-  const init = () => {
     this.data = {
-      ready: false,
+        ready: false,
     };
-  };
-  init();
 
-  this.handlers = {
-    pickPrice: (price) => {
-      this.event.trigger({
-        pickPrice: price,
-      });
-    },
-    setOrderbook: (baseBuying, counterSelling) => {
-      // If orderbook is already set, then this is a no-op
-      // Expects baseBuying and counterSelling to StellarSdk.Asset objects
-      if (this.data.ready && this.data.baseBuying.equals(baseBuying) && this.data.counterSelling.equals(counterSelling)) {
-        return;
-      }
+    this.handlers = {
+        pickPrice: (price) => {
+            this.event.trigger({
+                pickPrice: price,
+            });
+        },
 
-      if (this.data.close) {
-        this.data.close();
-      }
-      this.data = new MagicSpoon.Orderbook(driver.Server, baseBuying, counterSelling, () => {
-        this.event.trigger();
-        driver.session.forceUpdateAccountOffers();
-      });
-    },
-  };
+        setOrderbook: (baseBuying, counterSelling) => {
+            // If orderbook is already set, then this is a no-op
+            // Expects baseBuying and counterSelling to StellarSdk.Asset objects
+            const dataIsValid =
+                this.data.ready &&
+                this.data.baseBuying.equals(baseBuying) &&
+                this.data.counterSelling.equals(counterSelling);
+
+            if (dataIsValid) {
+                return;
+            } else if (this.data.close) {
+                this.data.close();
+            }
+
+            this.data = new MagicSpoon.Orderbook(driver.Server, baseBuying, counterSelling, () => {
+                this.event.trigger();
+                driver.session.forceUpdateAccountOffers();
+            });
+        },
+    };
 }
-
