8861eed7268ca658a9a7ee1f56c8bcfe23a4e19c
shiny-mountain <shiny-mountain@lobstr.co>
2019-07-05 12:10:26 +0300

    Fixed router bugs

--- src/components/Session/SessionContent/SessionContent.jsx
+++ src/components/Session/SessionContent/SessionContent.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import Driver from '../../../lib/Driver';
 import Inflation from './Inflation/Inflation';
 import Deposit from './Deposit/Deposit';
@@ -17,33 +17,32 @@ export default function SessionContent(props) {
 
     return (
         <React.Fragment>
-            <BrowserRouter>
-                <SessionAccountMenu d={d} />
-                <Switch>
-                    <Route exact path="/account/" component={prop => <SessionAccount {...prop} d={d} />} />
-                    <Route exact path="/account/addTrust/" component={prop => <Trust {...prop} d={d} />} />
-                    <Route
-                        exact
-                        path="/account/send/"
-                        component={prop => <ErrorBoundary><Send {...prop} d={d} /></ErrorBoundary>} />
-                    <Route
-                        exact
-                        path="/account/settings/"
-                        component={prop => <ErrorBoundary><Inflation {...prop} d={d} /></ErrorBoundary>} />
-                    <Route
-                        exact
-                        path="/account/multisig/"
-                        component={prop => <ErrorBoundary><Multisig {...prop} d={d} /></ErrorBoundary>} />
-                    <Route
-                        exact
-                        path="/account/history/"
-                        component={prop => <ErrorBoundary><History {...prop} d={d} /></ErrorBoundary>} />
-                    <Route
-                        exact
-                        path="/account/deposit/"
-                        component={prop => <ErrorBoundary><Deposit {...prop} d={d} /></ErrorBoundary>} />
-                </Switch>
-            </BrowserRouter>
+            <SessionAccountMenu d={d} />
+            <Switch>
+                <Route exact path="/account/" component={prop => <SessionAccount {...prop} d={d} />} />
+                <Route exact path="/account/addTrust/" component={prop => <Trust {...prop} d={d} />} />
+                <Route
+                    exact
+                    path="/account/send/"
+                    component={prop => <ErrorBoundary><Send {...prop} d={d} /></ErrorBoundary>} />
+                <Route
+                    exact
+                    path="/account/settings/"
+                    component={prop => <ErrorBoundary><Inflation {...prop} d={d} /></ErrorBoundary>} />
+                <Route
+                    exact
+                    path="/account/multisig/"
+                    component={prop => <ErrorBoundary><Multisig {...prop} d={d} /></ErrorBoundary>} />
+                <Route
+                    exact
+                    path="/account/history/"
+                    component={prop => <ErrorBoundary><History {...prop} d={d} /></ErrorBoundary>} />
+                <Route
+                    exact
+                    path="/account/deposit/"
+                    component={prop => <ErrorBoundary><Deposit {...prop} d={d} /></ErrorBoundary>} />
+                <Redirect from="/ledger/" to="/account/" />
+            </Switch>
         </React.Fragment>
     );
 }
