7f4aaddc9823c6cad1db8ee9cf156b3c3c127981
Gleb Pitsevich <pitsevich@gmail.com>
2019-07-22 18:12:05 +0300

    Update src/components/PopupAlert/PopupAlert.jsx

--- src/components/PopupAlert/PopupAlert.jsx
+++ src/components/PopupAlert/PopupAlert.jsx
@@ -44,7 +44,7 @@ export default class PopupAlert extends React.Component {
                 <div className={`popup-body ${animateBodyClass}`} key={`${op.id}-${op.offer_id}`}>
                     <div className="popup-icon-cell">{icon}</div>
                     <div className="popup-content-cell">
-                        <div className="popup-title">Traded</div>
+                        <div className="popup-title">Trade completed</div>
                         <div className="popup-text">
                             <div>Bought:</div>
                             <div>
