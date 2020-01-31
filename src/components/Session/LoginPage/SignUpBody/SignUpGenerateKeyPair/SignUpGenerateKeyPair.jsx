69b0539ed08b17c248afcebdfc687d73d2880d4f
shiny-mountain <shiny-mountain@lobstr.co>
2020-01-31 13:10:38 +0300

    Fixed import miss

--- src/components/Session/LoginPage/SignUpBody/SignUpGenerateKeyPair/SignUpGenerateKeyPair.jsx
+++ src/components/Session/LoginPage/SignUpBody/SignUpGenerateKeyPair/SignUpGenerateKeyPair.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import * as StellarSdk from 'stellar-sdk';
 import createStellarIdenticon from 'stellar-identicon-js';
 import images from '../../../../../images';
 import SignUpSecurityNotes from '../SignUpSecurityNotes/SignUpSecurityNotes';
