e4998de910cdcedb6bb89b34fccad5713767686b
shiny-mountain <shiny-mountain@lobstr.co>
2021-07-15 15:55:39 +0300

    Update the URL when searching for asset domain

--- src/components/Markets/Markets.jsx
+++ src/components/Markets/Markets.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Switch, Route, NavLink } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import * as StellarSdk from 'stellar-sdk';
 import Driver from '../../lib/Driver';
 import AssetList from '../Common/AssetList/AssetList';
@@ -13,58 +13,106 @@ import Stellarify from '../../lib/Stellarify';
 import NotFound from '../NotFound/NotFound';
 import images from '../../images';
 
+const KNOWN_MARKETS_ROUTE = '/markets/';
+const TOP_VOLUME_ROUTE = '/markets/top/';
+
 
 export default class Markets extends React.Component {
-    static getBaseAssetFromUrl() {
-        const slug = new URLSearchParams(window.location.search).get('base');
-        if (!slug) {
-            window.history.replaceState({}, '', '/markets/top?base=XLM-native');
-            return StellarSdk.Asset.native();
-        }
-        try {
-            return Stellarify.parseAssetSlug(slug);
-        } catch (e) {
-            console.warn(e);
-            return null;
-        }
-    }
     constructor(props) {
         super(props);
+        this.urlSearchParams = new URLSearchParams(window.location.search);
         this.state = {
             zeroAssetsVisible: false,
+            baseAsset: null,
         };
     }
 
+    componentDidMount() {
+        if (window.location.pathname === TOP_VOLUME_ROUTE) {
+            this.getBaseAssetFromUrl();
+        }
+
+        this.unlisten = this.props.history.listen(location => {
+            this.urlSearchParams = new URLSearchParams(location.search);
+
+            if (location.pathname === TOP_VOLUME_ROUTE) {
+                this.getBaseAssetFromUrl();
+            }
+
+            if (location.pathname === KNOWN_MARKETS_ROUTE) {
+                if (this.urlSearchParams.has('base')) {
+                    this.urlSearchParams.delete('base');
+                    this.props.history.replace(`?${this.urlSearchParams.toString()}`);
+                }
+            }
+        });
+    }
+
+    componentWillUnmount() {
+        this.unlisten();
+    }
+
+    getBaseAssetFromUrl() {
+        const slug = this.urlSearchParams.get('base');
+
+        if (this.state.baseAsset && Stellarify.assetToSlug(this.state.baseAsset) === slug) {
+            return;
+        }
+
+        if (!slug) {
+            this.urlSearchParams.set('base', 'XLM-native');
+            this.props.history.replace(`?${this.urlSearchParams.toString()}`);
+
+            this.setState({ baseAsset: StellarSdk.Asset.native() });
+            return;
+        }
+        try {
+            this.setState({ baseAsset: Stellarify.parseAssetSlug(slug) });
+        } catch (e) {
+            console.warn(e);
+            this.setState({ baseAsset: null });
+        }
+    }
+
+    toggleMarketsTables(isTop) {
+        this.props.history.push(`${isTop ? TOP_VOLUME_ROUTE : KNOWN_MARKETS_ROUTE}?${this.urlSearchParams.toString()}`);
+    }
+
     render() {
+        const { props } = this;
         const { d } = this.props;
         const { zeroAssetsVisible, fromStellarTicker } = this.state;
 
         return (
             <div>
                 <div className="so-back islandBack islandBack--t">
-                    <SearchByAnchor d={d} tradeLink />
+                    <SearchByAnchor
+                        d={d}
+                        tradeLink
+                        withUrlParams
+                        {...props}
+                    />
                 </div>
                 <div className="so-back islandBack">
                     <div className="island">
                         <div className="AssetList_Header">
                             <div className="AssetList_Tabs">
-                                <NavLink
-                                    to="/markets/"
-                                    exact
-                                    className="ListHeader_Title"
-                                    activeClassName="active">
+                                <div
+                                    onClick={() => this.toggleMarketsTables(false)}
+                                    className={`ListHeader_Title ${window.location.pathname === KNOWN_MARKETS_ROUTE ? 'active' : ''}`}
+                                >
                                     Known assets
-                                </NavLink>
-                                <NavLink
-                                    to="/markets/top/"
-                                    className="ListHeader_Title"
-                                    activeClassName="active">
+                                </div>
+                                <div
+                                    onClick={() => this.toggleMarketsTables(true)}
+                                    className={`ListHeader_Title ${window.location.pathname === TOP_VOLUME_ROUTE ? 'active' : ''}`}
+                                >
                                     Top volume markets
-                                </NavLink>
+                                </div>
                             </div>
                             <Switch>
                                 <Route
-                                    path="/markets/"
+                                    path={KNOWN_MARKETS_ROUTE}
                                     exact
                                     render={() =>
                                         <React.Fragment>
@@ -72,20 +120,23 @@ export default class Markets extends React.Component {
                                                 className="ListHeader_lowTradable"
                                                 onClick={() => this.setState({
                                                     zeroAssetsVisible: !fromStellarTicker && !zeroAssetsVisible,
-                                                })}>
+                                                })}
+                                            >
                                                 Include 24h zero volume assets
                                                 <input
                                                     type="checkbox"
                                                     readOnly
-                                                    checked={zeroAssetsVisible} />
+                                                    checked={zeroAssetsVisible}
+                                                />
                                                 <span className="custom-checkbox">
                                                     {zeroAssetsVisible && <img src={images['icon-tick-green']} alt="✓" />}
                                                 </span>
                                             </div>
                                         </React.Fragment>
-                                    } />
+                                    }
+                                />
                                 <Route
-                                    path="/markets/top/"
+                                    path={TOP_VOLUME_ROUTE}
                                     render={() =>
                                         <React.Fragment>
                                             <div className="ListHeader_dropdown">
@@ -93,36 +144,39 @@ export default class Markets extends React.Component {
                                                 <AssetDropDown
                                                     d={d}
                                                     compactSize
-                                                    asset={this.constructor.getBaseAssetFromUrl()}
-                                                    onUpdate={(asset) => {
+                                                    asset={this.state.baseAsset}
+                                                    onUpdate={asset => {
                                                         this.setState({ baseAsset: asset });
-                                                        window.history.pushState(
-                                                            {},
-                                                            null,
-                                                            `/markets/top?base=${Stellarify.assetToSlug(asset)}`,
-                                                        );
-                                                    }} />
+                                                        this.urlSearchParams.set('base', Stellarify.assetToSlug(asset));
+                                                        this.props.history.push(`?${this.urlSearchParams.toString()}`);
+                                                    }}
+                                                />
                                             </div>
                                         </React.Fragment>
-                                    } />
+                                    }
+                                />
                             </Switch>
                         </div>
                         <Switch>
                             <Route
-                                path="/markets/"
+                                path={KNOWN_MARKETS_ROUTE}
                                 exact
                                 render={() =>
                                     <AssetList
                                         d={d}
-                                        showLowTradable={zeroAssetsVisible} />
-                                } />
+                                        showLowTradable={zeroAssetsVisible}
+                                    />
+                                }
+                            />
                             <Route
-                                path="/markets/top/"
+                                path={TOP_VOLUME_ROUTE}
                                 render={() =>
                                     <TopVolumeAssets
                                         d={d}
-                                        baseAsset={this.constructor.getBaseAssetFromUrl()} />
-                                } />
+                                        baseAsset={this.state.baseAsset}
+                                    />
+                                }
+                            />
                             <Route render={() => <NotFound withoutWrapper />} />
                         </Switch>
 
@@ -134,7 +188,8 @@ export default class Markets extends React.Component {
                             <a
                                 href="https://github.com/stellarterm/stellarterm-directory"
                                 target="_blank"
-                                rel="nofollow noopener noreferrer">
+                                rel="nofollow noopener noreferrer"
+                            >
                                 please read the instructions on GitHub
                             </a>
                             .
@@ -155,4 +210,5 @@ export default class Markets extends React.Component {
 
 Markets.propTypes = {
     d: PropTypes.instanceOf(Driver).isRequired,
+    history: PropTypes.objectOf(PropTypes.any),
 };
