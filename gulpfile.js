eee5c55f41d857db84bea0834e6407c22822a3ae
shiny-mountain <shiny-mountain@lobstr.co>
2022-05-06 13:32:46 +0300

    Update node version to 12

--- gulpfile.js
+++ gulpfile.js
@@ -185,6 +185,7 @@ const bundler = watchify(browserify({
     },
 }).transform(babelify.configure({
     presets: ['@babel/preset-env', '@babel/preset-react'],
+    compact: true,
 })));
 
 if (process.env.WC_KEY) {
@@ -289,6 +290,7 @@ gulp.task('copyStaticFiles', () => gulp.src('static/**/*', { dot: true })
 gulp.task('uglify-js', () => gulp.src('dist/scripts/app.js')
     .pipe(babel({
         presets: ['@babel/preset-env'],
+        compact: true,
     }))
     .pipe($.uglify())
     .pipe(gulp.dest('dist/scripts')));
