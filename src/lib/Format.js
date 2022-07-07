1bbba761b5777f5229348cd443e6b563689f2595
shiny-mountain <shiny-mountain@lobstr.co>
2022-07-07 14:54:52 +0300

    Fix prefixes

--- src/lib/Format.js
+++ src/lib/Format.js
@@ -45,10 +45,9 @@ function nFormatter(num, digits) {
         { value: 1, symbol: '' },
         { value: 1e3, symbol: 'k' },
         { value: 1e6, symbol: 'M' },
-        { value: 1e9, symbol: 'G' },
+        { value: 1e9, symbol: 'B' },
         { value: 1e12, symbol: 'T' },
-        { value: 1e15, symbol: 'P' },
-        { value: 1e18, symbol: 'E' },
+        { value: 1e15, symbol: 'Q' },
     ];
     const rx = /\.0+$|(\.[0-9]*[1-9])0+$/;
     const item = lookup.slice().reverse().find(i => num >= i.value);
