50383dffa969da873582a708b720de4636fe1dea
shiny-mountain <shiny-mountain@lobstr.co>
2023-01-19 15:24:25 +0300

    If the request to Vault API failed, go to the "manually sign" flow

--- src/components/Common/SignChallengeBlock/SignChallengeWithVault/SignChallengeWithVault.jsx
+++ src/components/Common/SignChallengeBlock/SignChallengeWithVault/SignChallengeWithVault.jsx
@@ -1,52 +1,37 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import * as StellarSdk from 'stellar-sdk';
 import images from '../../../../images';
 import {
     PROVIDER_DATA,
     THRESHOLD_MULTIPLIER,
-    VAULT_NOT_ALLOW_UNSIGNED_TX_ERROR,
 } from '../../../../lib/constants/multisigConstants';
-import Ellipsis from '../../Ellipsis/Ellipsis';
 import Driver from '../../../../lib/driver/Driver';
 import Multisig from '../../../../lib/driver/driverInstances/Multisig';
+import useIsMounted from '../../../../lib/hooks/useIsMounted';
 
 
 const SignChallengeWithVault = ({ d, resolver, tx }) => {
     const { multisigProvider, requiredSigners } = d.multisig;
 
     const hash = tx.hash().toString('hex');
-    const xdr = tx.toEnvelope().toXDR('base64');
 
-    const [isTxSentToVault, setIsTxSentToVault] = useState(null);
     const [hasAllSignatures, setHasAllSignatures] = useState(false);
     const [vaultRequestPending, setVaultRequestPending] = useState(false);
     const [signedTx, setSignedTx] = useState(null);
 
-    useEffect(() => {
-        Multisig.sendXdrToVault(xdr)
-            .then(() => {
-                setIsTxSentToVault(true);
-            })
-            .catch(({ data, message }) => {
-                if (data && data.detail !== VAULT_NOT_ALLOW_UNSIGNED_TX_ERROR) {
-                    d.toastService.error('Error', message);
-                }
-                if (data && data.detail === VAULT_NOT_ALLOW_UNSIGNED_TX_ERROR) {
-                    d.toastService.error('Enable the option "Allow Unsigned Transactions" in the  LOBSTR app');
-                }
-                setIsTxSentToVault(false);
-            });
-    }, []);
+    const isMounted = useIsMounted();
 
-    const checkTransaction = () => {
+    const checkTransaction = useCallback(() => {
         if (hasAllSignatures) {
             resolver(new StellarSdk.Transaction(signedTx.xdr, signedTx.network_passphrase));
+            return;
         }
         if (vaultRequestPending) {
             return;
         }
         setVaultRequestPending(true);
+
         Multisig.getChallengeTxFromVault(hash).then(res => {
             const weight = d.multisig.getSignaturesWeight(res.xdr, res.network_passphrase);
             setVaultRequestPending(false);
@@ -56,19 +41,19 @@ const SignChallengeWithVault = ({ d, resolver, tx }) => {
                 setSignedTx(res);
             }
         });
-    };
+    }, [hasAllSignatures, vaultRequestPending, signedTx]);
 
     useEffect(() => {
-        if (!setIsTxSentToVault || hasAllSignatures) {
+        if (hasAllSignatures || !isMounted.current) {
             return;
         }
         const interval = setInterval(() => {
             checkTransaction();
         }, 5000);
 
-            // eslint-disable-next-line consistent-return
+        // eslint-disable-next-line consistent-return
         return () => { clearInterval(interval); };
-    }, [setIsTxSentToVault, hasAllSignatures]);
+    }, [hasAllSignatures, isMounted]);
 
 
     return (
@@ -91,36 +76,28 @@ const SignChallengeWithVault = ({ d, resolver, tx }) => {
                 </div>
             </div>
             <div className="SignChallengeBlock_footer">
-                {isTxSentToVault === null && <span>Sending the transaction to the Vault<Ellipsis /></span>}
+                {vaultRequestPending && !hasAllSignatures &&
+                    <div className="spinner-green">
+                        <div className="nk-spinner" />
+                    </div>}
+                {!vaultRequestPending && !hasAllSignatures &&
+                    <img src={images['24-icon-warning-triangle']} alt="" />}
+                {hasAllSignatures && <img src={images['24-icon-success-circle']} alt="" />}
 
-                {isTxSentToVault === true &&
+                <div className="SignChallengeBlock_required-signers">
+                    {hasAllSignatures ?
+                        <span>Signed by LOBSTR Vault co-signers</span> :
+                        <React.Fragment>
+                            <span>{requiredSigners} signatures required</span>
+                            <span>This account and {requiredSigners - 1} LOBSTR Vault co-signers</span>
+                        </React.Fragment>}
 
-                            <React.Fragment>
-                                {vaultRequestPending && !hasAllSignatures &&
-                                    <div className="spinner-green">
-                                        <div className="nk-spinner" />
-                                    </div>}
-                                {!vaultRequestPending && !hasAllSignatures &&
-                                    <img src={images['24-icon-warning-triangle']} alt="" />}
-                                {hasAllSignatures && <img src={images['24-icon-success-circle']} alt="" />}
+                </div>
 
-                                <div className="SignChallengeBlock_required-signers">
-                                    {hasAllSignatures ?
-                                        <span>Signed by LOBSTR Vault co-signers</span> :
-                                        <React.Fragment>
-                                            <span>{requiredSigners} signatures required</span>
-                                            <span>This account and {requiredSigners - 1} LOBSTR Vault co-signers</span>
-                                        </React.Fragment>}
-
-                                </div>
-
-                                <button className="s-button SignChallengeBlock_button" onClick={() => checkTransaction()}>
-                                    {!hasAllSignatures && <img src={images['icon-retry']} alt="" />}
-                                    {hasAllSignatures ? 'Continue' : 'Retry'}
-                                </button>
-                            </React.Fragment>}
-
-                {isTxSentToVault === false && <span>Error sent</span>}
+                <button className="s-button SignChallengeBlock_button" onClick={() => checkTransaction()}>
+                    {!hasAllSignatures && <img src={images['icon-retry']} alt="" />}
+                    {hasAllSignatures ? 'Continue' : 'Retry'}
+                </button>
             </div>
         </React.Fragment>
     );
