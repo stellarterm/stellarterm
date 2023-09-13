18520c3810994fffeebbaeb2e3bd66790258c53c
ebarimjesus <130609758+ebarimjesus@users.noreply.github.com>
2023-09-13 07:03:35 +0100

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -192,14 +192,22 @@ class Header extends React.Component {
                             <Link className="Nav_logo" to={'/'}>
                                 ZingyTrader
                             </Link>
-                            <a
-                               className="Header_link"
-                               href="https://zingypay.com/zingypay/payment-form"
-                               target="_blank"
-                               rel="noopener noreferrer"
-                             >
-                               Buy AFRO <img src="https://res.cloudinary.com/dp7civtid/image/upload/v1694582789/mastercard-icon_nbzeds.png" alt="MasterCard Icon" style={{ maxWidth: '16px', maxHeight: '16px' }} />
-                             </a>
+                            <div className="Header_link_container">
+                               <a
+                                    href="https://zingypay.com/zingypay/payment-form"
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                    className="Header_link"
+                               >
+                                   Buy AFRO
+                               </a>
+                               <img
+                                   src="https://res.cloudinary.com/dp7civtid/image/upload/v1694582789/mastercard-icon_nbzeds.png"
+                                   alt="MasterCard Icon"
+                                   style={{ maxWidth: '16px', maxHeight: '16px', verticalAlign: 'middle' }}
+                               />
+                            </div>
+
                             {this.createHeaderTab('/markets/', 'Markets')}
                             {this.createHeaderTab('/exchange/', 'Exchange')}
                             {this.createHeaderTab('/swap/', 'Swap')}
